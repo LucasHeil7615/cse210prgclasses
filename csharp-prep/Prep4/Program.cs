2ede19fe468965997f5e31e51b0b484f07c0fc51
Lucas Heil <lightnight7615@gmail.com>
2024-01-17 16:23:35 -0700

    prep 4 first attempt

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        int num1 = 0
+        List<int> numbers = new List<int>();
+        Console.writeline("Welcome to list calculator. Please enter you list of numbers. ")
+        cosole.writeline("We will give you the sum, average, and the largest number. ")
+        console.writeline("When you are finished please enter 0 and your results will be displayed. ")
+        do
+        {
+            Console.write("Enter a Number: ")
+            num1 int.Parse(Console.ReadLine());
+            numbers.add(num1);
+            
+        } while (num1 != 0);
+
+        Console.writeline($"The sum is: {numbers.Count}")
+    } 
 }
+// Finished product
+//          Enter a list of numbers, type 0 when finished.
+//   Enter number: 18
+//   Enter number: 36
+//   Enter number: 2
+//   Enter number: 48
+//   Enter number: 6
+//   Enter number: 12
+//   Enter number: 9
+//   Enter number: 0
+//   The sum is: 131
+//   The average is: 18.714285714285715
+//   The largest number is: 48
