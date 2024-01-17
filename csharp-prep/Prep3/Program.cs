81eb85b062f10dc42e75475c306436e094342941
Lucas Heil <lightnight7615@gmail.com>
2024-01-17 10:56:30 -0700

    Prep 3 Step 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,29 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    static void Main(string[] args){
+        //  Start by asking the user for the magic number.
+        Console.Write("What is the magic number? ");
+        int Magic_number = Console.ReadLine();
+        // Ask the user for a guess.
+        Console.Write("What is your guess? ");
+        int Guess_num = Console.ReadLine();
+        // Using an if statement, determine if the user needs to guess higher or lower next time, or tell them if they guessed it.
+        if (Magic_number > Guess_num){
+            Console.WriteLine("Guess Higher.");
+        }
+        else if (Magic_number < Guess_num){
+            Console.WriteLine("Guess Lower.");
+        }
+        else
+        {
+            Console.WriteLine("Congrats you guessed the number :)");
+        }
+        }
+
+        
     }
-}
+ 
+
+
+
