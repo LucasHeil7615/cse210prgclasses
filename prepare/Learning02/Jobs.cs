a8f19b1ed1e05b4f7a66c2d646ce2bc7a59ec96f
Lucas Heil <lightnight7615@gmail.com>
2024-01-22 12:04:58 -0700

    Began Abstration project

---
+++ prepare/Learning02/Jobs.cs
@@ -0,0 +1,15 @@
+public class Job{
+
+    string _company;
+
+    string -jobTitle;
+
+    int _startYear;
+
+    int _endYear;
+    
+    public void Display(){
+        // Software Engineer (Miscrosoft) 2019-2022
+        Console.WriteLine($"{-jobTitle} ({_company}) {startYear}-{_endYear}");
+    }
+}
